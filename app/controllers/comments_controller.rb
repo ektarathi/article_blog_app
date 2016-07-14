925040207c37d7831039d8c4e88ebe1506682261
Ekta Rathi <ekta.rathi@dubitlimited.com>
2016-07-14 16:23:30 +0100

    Fix styling issues

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,21 @@
 class CommentsController < ApplicationController
   
-  before_action :set_comment, only: [:destroy]
-
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(params[:comment].permit(:name, :body))
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
     redirect_to article_path(@article)
   end
 
   private
 
-  def set_comment
-    @article = Article.find(params[:id])
-    @comment = @article.comments.find(params[:id])
+  def comment_params
+    params.require(:comment).permit(:name, :body)
   end
 end
