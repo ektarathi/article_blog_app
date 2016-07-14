d5968e584c6eaf01a52c008ad9325569e3ddd8e4
Ekta Rathi <ekta.rathi@dubitlimited.com>
2016-07-14 15:16:17 +0100

    Updated domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :domain => 'localhost:3000',
+      :domain => 'gmail.com',
       :user_name => "ekta.rathi@dubitlimited.com",
       :password => "tvovjzhyrunawwff",
       :authentication => :plain,
