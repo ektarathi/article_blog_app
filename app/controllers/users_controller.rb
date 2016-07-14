41749d6bdcb92316da15a3838783e5a154853113
Ekta Rathi <ekta.rathi@dubitlimited.com>
2016-07-14 15:08:21 +0100

    Updated forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
       flash[:notice] = 'User was successfully subscribed.'
       redirect_to_js root_path 
     else
-      @user.errors.add(:email, 'is not correct')
+      @user.errors
       render :new
     end
   end
